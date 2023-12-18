19f908ddb71dcd87f93584bfff94ca6a358525ec
DILPREET1910 <dsahney1910@gmail.com>
2023-12-18 09:18:11 +0530

    adder ui

--- app/dashboard/add/page.tsx
+++ app/dashboard/add/page.tsx
@@ -1,5 +1,30 @@
 export default function Add() {
   return(
-    <h1>Add</h1>
+    <div className="border-black border-4 rounded-xl p-12">
+      <form>
+        <div className="flex flex-row">
+          <h1>date picker</h1>
+          <h1 className="ml-auto">category picker</h1>
+        </div>
+
+        <div className="pt-8">
+          <label className="block ml-1">Amount*</label>
+          <input required  name="amount" 
+            className="border-2 border-black rounded-lg p-0.5 pl-1 w-full" 
+            placeholder="100₹" type="number"/>
+        </div>
+
+        <div className="pt-4">
+          <label className="block ml-1">Description</label>
+          <input name="description" 
+            className="border-2 border-black rounded-lg p-0.5 pl-1 w-full" 
+            placeholder="description if any" type="text"/>
+        </div>
+
+        <div className="pt-8">
+          <button type="submit" className="bg-gray-700 text-white shadow hover:bg-gray-900 rounded-lg w-full p-1">Add</button>
+        </div>
+      </form>
+    </div>
   );
 }
