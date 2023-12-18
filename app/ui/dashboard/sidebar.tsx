9b70e685ee602e71c19afcf71fc74f8bb7f8f9b5
DILPREET1910 <dsahney1910@gmail.com>
2023-12-18 08:55:07 +0530

    sidebar clsx

--- app/ui/dashboard/sidebar.tsx
+++ app/ui/dashboard/sidebar.tsx
@@ -5,6 +5,10 @@ import { UserButton, useUser } from "@clerk/nextjs";
 
 // next imports
 import Link from "next/link";
+import { usePathname } from "next/navigation"; 
+
+// clsx imports
+import clsx from "clsx";
 
 export default function Sidebar(){
   const {user,isLoaded} = useUser();
@@ -15,12 +19,18 @@ export default function Sidebar(){
     {key:3, name:"Data Entries",href:"/dashboard/entries"},
   ];
 
+  const pathname = usePathname();
+
   return(
     <div className="flex h-full flex-col px-3 py-4 md:px-2 shadow">
       {
         links.map((element)=>{
           return (
-            <Link key={element.key} href={element.href}>{element.name}</Link>
+            <Link key={element.key} href={element.href}
+              className={clsx( "p-2 rounded-lg hover:bg-sky-50" ,
+                {"bg-sky-200":pathname==element.href},
+              )}
+            >{element.name}</Link>
           );
         })
       }
