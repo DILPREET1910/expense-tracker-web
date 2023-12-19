54ebc6c1c0432f71af17b20ac7fcf70a74c49279
DILPREET1910 <dsahney1910@gmail.com>
2023-12-19 21:43:42 +0530

    category selector

--- app/dashboard/add/page.tsx
+++ app/dashboard/add/page.tsx
@@ -1,10 +1,40 @@
-export default function Add() {
+// project imports
+import { GetCategories } from "@/app/lib/categories";
+
+// clerk imports
+import { auth } from "@clerk/nextjs";
+
+export default async function Add() {
+  // get user id
+  const {userId}=auth();
+
+  // get categories for user
+  const categories = await GetCategories(userId!);
+
   return(
     <div className="border-black border-4 rounded-xl p-12">
       <form>
         <div className="flex flex-row">
           <h1>date picker</h1>
-          <h1 className="ml-auto">category picker</h1>
+          <select 
+            className="ml-auto bg-white" 
+            required
+            defaultValue={""}
+          >
+            <option disabled value={""}>Select Category</option>
+            {
+              categories.map((category)=>{
+                return(
+                  <option 
+                    key={category.id}
+                    value={category.id}
+                  >
+                    {category.name}
+                  </option>
+                );
+              })
+            }
+          </select>
         </div>
 
         <div className="pt-8">
