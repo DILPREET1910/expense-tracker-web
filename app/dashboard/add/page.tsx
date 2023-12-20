89e7aad7d55148d67ca8cb5a2f9bc89f990f6c16
DILPREET1910 <dsahney1910@gmail.com>
2023-12-20 10:20:35 +0530

    date picker

--- app/dashboard/add/page.tsx
+++ app/dashboard/add/page.tsx
@@ -1,5 +1,6 @@
 // project imports
 import { GetCategories } from "@/app/lib/categories";
+import AdderForm from "@/app/ui/dashboard/add/adderForm";
 
 // clerk imports
 import { auth } from "@clerk/nextjs";
@@ -13,48 +14,7 @@ export default async function Add() {
 
   return(
     <div className="border-black border-4 rounded-xl p-12">
-      <form>
-        <div className="flex flex-row">
-          <h1>date picker</h1>
-          <select 
-            className="ml-auto bg-white" 
-            required
-            defaultValue={""}
-          >
-            <option disabled value={""}>Select Category</option>
-            {
-              categories.map((category)=>{
-                return(
-                  <option 
-                    key={category.id}
-                    value={category.id}
-                  >
-                    {category.name}
-                  </option>
-                );
-              })
-            }
-          </select>
-        </div>
-
-        <div className="pt-8">
-          <label className="block ml-1">Amount*</label>
-          <input required  name="amount" 
-            className="border-2 border-black rounded-lg p-0.5 pl-1 w-full" 
-            placeholder="100₹" type="number"/>
-        </div>
-
-        <div className="pt-4">
-          <label className="block ml-1">Description</label>
-          <input name="description" 
-            className="border-2 border-black rounded-lg p-0.5 pl-1 w-full" 
-            placeholder="description if any" type="text"/>
-        </div>
-
-        <div className="pt-8">
-          <button type="submit" className="bg-gray-700 text-white shadow hover:bg-gray-900 rounded-lg w-full p-1">Add</button>
-        </div>
-      </form>
+      <AdderForm categories={categories}/>
     </div>
   );
 }
---
+++ app/ui/dashboard/add/adderForm.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+// react imports
+import React, { useState } from "react";
+
+// react datepicker
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'
+
+
+// vercel imports
+import { QueryResultRow } from "@vercel/postgres";
+
+export default function AdderForm({categories}:{categories:QueryResultRow[]}){
+  const [date,setDate] = useState(new Date());
+
+  return(
+    <form>
+      <div className="flex flex-row">
+        <DatePicker selected={date} onChange={(newDate)=>setDate(newDate!)}/>
+        <select 
+          className="ml-auto bg-white" 
+          required
+          defaultValue={""}
+        >
+          <option disabled value={""}>Select Category</option>
+          {
+            categories.map((category)=>{
+              return(
+                <option 
+                  key={category.id}
+                  value={category.id}
+                >
+                  {category.name}
+                </option>
+              );
+            })
+          }
+        </select>
+      </div>
+
+      <div className="pt-8">
+        <label className="block ml-1">Amount*</label>
+        <input required  name="amount" 
+          className="border-2 border-black rounded-lg p-0.5 pl-1 w-full" 
+          placeholder="100₹" type="number"/>
+      </div>
+
+      <div className="pt-4">
+        <label className="block ml-1">Description</label>
+        <input name="description" 
+          className="border-2 border-black rounded-lg p-0.5 pl-1 w-full" 
+          placeholder="description if any" type="text"/>
+      </div>
+
+      <div className="pt-8">
+        <button type="submit" className="bg-gray-700 text-white shadow hover:bg-gray-900 rounded-lg w-full p-1">Add</button>
+      </div>
+    </form>
+  );
+}
