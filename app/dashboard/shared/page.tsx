b3cbf30b5b329e9c89e4cf582cb8e2d9f24d48fe
DILPREET1910 <dsahney1910@gmail.com>
2024-01-29 21:47:23 +0530

    add public key on shared page

--- app/dashboard/shared/page.tsx
+++ app/dashboard/shared/page.tsx
@@ -1,3 +1,21 @@
 export default function SharedWithYou() {
-  return <div>Shared with your</div>;
+  return (
+    <div>
+      <p className="inline-block text-xl font-bold">Add public key</p>
+      <div>
+        <input
+          type="text"
+          name="public_key"
+          className="p-0.5 border-2 border-gray-900 rounded-lg"
+          placeholder="enter public key"
+        />
+        <button
+          type="submit"
+          className="ml-2 bg-gray-700 hover:bg-gray-900 rounded-lg p-1 text-white shadow"
+        >
+          add
+        </button>
+      </div>
+    </div>
+  );
 }
