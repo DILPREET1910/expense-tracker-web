994c899ef0f2083eb2f8034907be3f52e9331dc5
DILPREET1910 <dsahney1910@gmail.com>
2024-01-29 16:41:27 +0530

    public key

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,8 +1,16 @@
+// clerk imports
+import { auth } from "@clerk/nextjs";
+
 // react icons imports
 import CopyPublicKey from "@/app/ui/dashboard/profile/copyPublicKey";
 import { RiInformationLine } from "react-icons/ri";
+import { GetPublicKey } from "@/app/lib/user";
+import { useId } from "react";
 
-export default function Profile() {
+export default async function Profile() {
+  const { userId } = auth();
+
+  const publicKey = await GetPublicKey({ id: userId! });
   return (
     <div>
       <p className="inline-block">Public key</p>
@@ -12,7 +20,7 @@ export default function Profile() {
       >
         <RiInformationLine className="text-gray-500 hover:text-black" />
       </div>
-      <CopyPublicKey publicKey={"12034"} />
+      <CopyPublicKey publicKey={publicKey} />
     </div>
   );
 }
--- app/lib/user.ts
+++ app/lib/user.ts
@@ -1,52 +1,68 @@
-// vercel imports 
+// vercel imports
 import { sql } from "@vercel/postgres";
 
-export async function CheckUserExists(id:string){
-  try{
+// uuid generator imports
+import { v4 as uuidv4 } from "uuid";
+
+export async function CheckUserExists(id: string) {
+  try {
     const result = await sql`SELECT EXISTS(SELECT 1 FROM users WHERE id=${id})`;
     const row = result.rows[0];
     return row.exists;
-  }catch(error){
+  } catch (error) {
     console.log(`Error: while checking user exists : ${error}`);
-  } 
+  }
 
   return false;
 }
 
-export async function InsertUser(id:string){
-  try{
-    await sql`INSERT INTO users (id, first_time) 
-VALUES(${id},TRUE)`;
+export async function InsertUser(id: string) {
+  try {
+    await sql`INSERT INTO users (id, first_time, public_key) 
+VALUES(${id},TRUE, ${uuidv4()})`;
     return true;
-  }catch(error){
+  } catch (error) {
     console.log(`Error: while inserting user : ${error}`);
-  } 
+  }
 
   return false;
 }
 
-export async function CheckFirstTime(id:string){
-  try{
+export async function CheckFirstTime(id: string) {
+  try {
     const result = await sql`SELECT * FROM users WHERE id=${id}`;
     const row = result.rows[0];
     return row.first_time;
-  }catch(error){
+  } catch (error) {
     console.log(`Error: while checking user first time : ${error}`);
   }
 
   return false;
 }
 
-export async function ToggleFirstTime(id:string){
-  try{
+export async function ToggleFirstTime(id: string) {
+  try {
     const result = await sql`SELECT * FROM users WHERE id=${id}`;
     const row = result.rows[0];
     const first_time = !row.first_time;
     await sql`UPDATE users SET first_time=${first_time} WHERE id=${id}`;
     return true;
-  }catch(error){
+  } catch (error) {
     console.log(`Error: while toggling user first time : ${error}`);
   }
 
   return false;
+}
+
+export async function GetPublicKey({ id }: { id: string }) {
+  try {
+    const result = await sql`SELECT * FROM users WHERE id=${id}`;
+    const row = result.rows[0];
+    const publicKey = row.public_key;
+    return publicKey;
+  } catch (error) {
+    console.log(`Error: while getting public key: ${error}`);
+  }
+
+  return "";
 }
