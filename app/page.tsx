9f0c0361ce8337928fc7450fa76c49e93c0d31e9
DILPREET1910 <dsahney1910@gmail.com>
2024-02-06 12:51:25 +0530

    sign in button at home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 // clerk imports
-import { auth } from "@clerk/nextjs"
+import { SignInButton, auth } from "@clerk/nextjs";
 
 //next js imports
 import { redirect } from "next/navigation";
 
 export default function Home() {
   // redirect to dashboard if user logged in
-  const {userId} = auth();
-  if(userId){
+  const { userId } = auth();
+  if (userId) {
     redirect("/dashboard");
   }
 
   return (
-    <h1>Home</h1>
-  )
+    <div>
+      <h1>Home</h1>
+      <SignInButton />
+    </div>
+  );
 }
